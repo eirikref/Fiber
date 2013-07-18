830ad78005fe4624f369eff76338aa3b362d38fa
Eirik Refsdal <eirikref@gmail.com>
2013-07-18 13:27:53 +0200

    Adding simple method for getting list of registrered data types

--- src/Fiber/Fiber.php
+++ src/Fiber/Fiber.php
@@ -13,7 +13,7 @@ namespace Fiber;
  * Main class used for generating data of multiple data types
  *
  * @package Fiber
- * @version 2013-07-16
+ * @version 2013-07-18
  * @author  Eirik Refsdal <eirikref@gmail.com>
  */
 class Fiber extends \Fiber\DataType
@@ -26,6 +26,21 @@ class Fiber extends \Fiber\DataType
      * @access private
      */
     private $types = array("ArrayType");
+
+
+
+    /**
+     * Helper method for getting list of registered data types
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-07-18
+     * @access public
+     * @return array
+     */
+    public function getTypes()
+    {
+        return $this->types;
+    }
 
 
 
---
+++ tests/phpunit/Fiber/GetTypesTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Fiber: Unit tests
+ * Copyright (c) 2013 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Fiber\Tests\Fiber;
+
+/**
+ * Fiber: Unit tests for Fiber::getTypes()
+ *
+ * @package    Fiber
+ * @subpackage Tests
+ * @version    2013-07-18
+ * @author     Eirik Refsdal <eirikref@gmail.com>
+ */
+class GetTypesTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Check default types
+     *
+     * @test
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-07-18
+     * @access public
+     * @covers \Fiber\Fiber::getTypes
+     */
+    public function getDefaultTypes()
+    {
+        $fiber   = new \Fiber\Fiber();
+        $defSize = 1;
+
+        $this->assertTrue(is_array($fiber->getTypes()));
+        $this->assertEquals(1, count($fiber->getTypes()));
+    }
+}
