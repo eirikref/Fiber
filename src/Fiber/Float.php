22296144caae11b979e4cbdbd4c944561de5bd22
Eirik Refsdal <eirikref@gmail.com>
2013-07-18 14:58:11 +0200

    Add Float::getZero()

--- src/Fiber/Float.php
+++ src/Fiber/Float.php
@@ -9,12 +9,36 @@ namespace Fiber;
 /**
  * Fiber: Float
  *
- * Class for generating float test data
+ * Class for generating integer test data
  *
  * @package Fiber
- * @version 2013-06-27
+ * @version 2013-07-18
  * @author  Eirik Refsdal <eirikref@gmail.com>
  */
 class Float extends DataType
 {
+
+    /**
+     * List of available generators for the given data type, with
+     * their corresponding action/method
+     *
+     * @var    array $generators
+     * @access protected
+     */
+    protected $generators = array("zero" => "getZero");
+
+
+
+    /**
+     * Get zero
+     *
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-07-18
+     * @access public
+     * @return float
+     */
+    public function getZero()
+    {
+        return 0.0;
+    }
 }
---
+++ tests/phpunit/Float/getZeroTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Fiber: Unit tests
+ * Copyright (c) 2013 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Fiber\Tests\Float;
+
+/**
+ * Fiber: Unit tests for Float::getZero()
+ *
+ * @package    Fiber
+ * @subpackage Tests
+ * @version    2013-07-18
+ * @author     Eirik Refsdal <eirikref@gmail.com>
+ */
+class getZeroTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Check getZero() directly
+     *
+     * @test
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-07-18
+     * @access public
+     * @covers \Fiber\Float::getZero
+     */
+    public function getZeroDirectly()
+    {
+        $float = new \Fiber\Float();
+        $this->assertEquals(0.0, $float->getZero());
+        $this->assertTrue(is_float($float->getZero()));
+    }
+}
