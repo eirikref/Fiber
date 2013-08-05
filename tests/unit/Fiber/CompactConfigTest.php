2dc27e615bccf9dbc45a0560fff6f5a121f4a6e4
Eirik Refsdal <eirikref@gmail.com>
2013-08-05 14:09:45 +0200

    Started implementing compact config format

---
+++ tests/unit/Fiber/CompactConfigTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Fiber: Unit tests
+ * Copyright (c) 2013 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Fiber\Tests\Fiber;
+
+/**
+ * Fiber: Unit tests for Fiber::get()
+ *
+ * @package    Fiber
+ * @subpackage Tests
+ * @version    2013-08-05
+ * @author     Eirik Refsdal <eirikref@gmail.com>
+ */
+class CompactConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Make sure the compact config format passes through and ends up
+     * the way we expect it to
+     *
+     * @test
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-08-05
+     * @access public
+     * @covers \Fiber\Fiber::get
+     */
+    public function getWithInvalidConfig()
+    {
+        $fiber    = new \Fiber\Fiber();
+        $config   = "bool";
+        $expected = array(array(true), array(false));
+
+        $this->assertEquals($expected, $fiber->get($config));
+    }
+}
---
+++ tests/unit/Fiber/ParseCompactConfigTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Fiber: Unit tests
+ * Copyright (c) 2013 Eirik Refsdal <eirikref@gmail.com>
+ */
+
+namespace Fiber\Tests\Fiber;
+
+/**
+ * Fiber: Unit tests for Fiber::parseCompactConfig()
+ *
+ * @package    Fiber
+ * @subpackage Tests
+ * @version    2013-08-05
+ * @author     Eirik Refsdal <eirikref@gmail.com>
+ */
+class ParseCompactConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Data provider for invalid compact config format strings
+     * 
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-08-05
+     * @access public
+     * @return array
+     */
+    public function getInvalidCompactConfig()
+    {
+        return array(array("",),
+                     array("doesnotexist")
+        );
+    }
+
+
+
+    /**
+     * Make sure invalid config formats fail
+     *
+     * @test
+     * @author       Eirik Refsdal <eirikref@gmail.com>
+     * @since        2013-08-05
+     * @access       public
+     * @covers       \Fiber\Fiber::parseCompactConfig
+     * @dataProvider getInvalidCompactConfig
+     *
+     * @param        string $config
+     */
+    public function makeSureInvalidParamsFail($config)
+    {
+        $expected = array();
+        $mock     = $this->getMockForAbstractClass("\Fiber\Fiber");
+        $method   = new \ReflectionMethod($mock, "parseCompactConfig");
+        $method->setAccessible(true);
+
+        $res = $method->invokeArgs($mock, array($config));
+
+        $this->assertEquals($expected, $res);
+    }
+
+
+
+    /**
+     * Get just booleans
+     *
+     * @test
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-08-05
+     * @access public
+     * @covers \Fiber\Fiber::parseCompactConfig
+     */
+    public function getOnlyBooleans()
+    {
+        $config   = "bool";
+        $expected = array("include" => "bool");
+        $mock     = $this->getMockForAbstractClass("\Fiber\Fiber");
+        $method   = new \ReflectionMethod($mock, "parseCompactConfig");
+        $method->setAccessible(true);
+
+        $res = $method->invokeArgs($mock, array($config));
+
+        $this->assertEquals($expected, $res);
+    }
+
+
+
+    /**
+     * Get anything but booleans
+     *
+     * @test
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-08-05
+     * @access public
+     * @covers \Fiber\Fiber::parseCompactConfig
+     */
+    public function getAnythingButBooleans()
+    {
+        $config   = "!bool";
+        $expected = array("exclude" => "bool");
+        $mock     = $this->getMockForAbstractClass("\Fiber\Fiber");
+        $method   = new \ReflectionMethod($mock, "parseCompactConfig");
+        $method->setAccessible(true);
+
+        $res = $method->invokeArgs($mock, array($config));
+
+        $this->assertEquals($expected, $res);
+    }
+}
