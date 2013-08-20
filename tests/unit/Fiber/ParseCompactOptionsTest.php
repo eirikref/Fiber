d5cb0534d810cec6a3088e29946fab987518a1b1
Eirik Refsdal <eirikref@gmail.com>
2013-08-20 13:07:08 +0200

    Test charset and combined parsing

--- tests/unit/Fiber/ParseCompactOptionsTest.php
+++ tests/unit/Fiber/ParseCompactOptionsTest.php
@@ -78,4 +78,86 @@ class ParseCompactOptionsTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $res);
     }
+
+
+
+    /**
+     * Data provider for charsets (and similar). Not quite sure just
+     * yet what I should include/allow here and not.
+     * 
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-08-20
+     * @access public
+     * @return array
+     */
+    public function getCharsets()
+    {
+        return array(array("utf8"),
+                     array("latin1"));
+    }
+
+
+
+    /**
+     * Make sure we are able to parse charset
+     *
+     * @test
+     * @author       Eirik Refsdal <eirikref@gmail.com>
+     * @since        2013-08-20
+     * @access       public
+     * @covers       \Fiber\Fiber::parseCompactOptions
+     * @dataProvider getCharsets
+     *
+     * @param        string $config
+     */
+    public function testCharsetParsing($config)
+    {
+        $expected = array("charset" => $config);
+        $mock     = $this->getMockForAbstractClass("\Fiber\Fiber");
+        $method   = new \ReflectionMethod($mock, "parseCompactOptions");
+        $method->setAccessible(true);
+
+        $this->assertEquals($expected, $method->invokeArgs($mock, array($config)));
+    }
+
+
+
+    /**
+     * Data provider for combined otions
+     * 
+     * @author Eirik Refsdal <eirikref@gmail.com>
+     * @since  2013-08-20
+     * @access public
+     * @return array
+     */
+    public function getCombinedData()
+    {
+        return array(array("utf8:1-1000",
+                           array("charset" => "utf8", "min" => 1, "max" => 1000)),
+                     array("1-5:latin1",
+                           array("min" => 1, "max" => 5, "charset" => "latin1")));
+    }
+
+
+
+    /**
+     * Make sure we are able to parse charset
+     *
+     * @test
+     * @author       Eirik Refsdal <eirikref@gmail.com>
+     * @since        2013-08-20
+     * @access       public
+     * @covers       \Fiber\Fiber::parseCompactOptions
+     * @dataProvider getCombinedData
+     *
+     * @param        string $config
+     */
+    public function testCombinedParsing($config, $expected)
+    {
+        $mock   = $this->getMockForAbstractClass("\Fiber\Fiber");
+        $method = new \ReflectionMethod($mock, "parseCompactOptions");
+        $method->setAccessible(true);
+
+        $this->assertEquals($expected, $method->invokeArgs($mock, array($config)));
+    }
 }
